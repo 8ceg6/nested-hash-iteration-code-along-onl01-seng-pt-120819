4fc7242b1d2642dc4d78af9024d3b7f541111c6f
cedric e goddard <8ceg6@comcast.net>
2019-12-21 01:16:51 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,6 +18,7 @@ def remove_strawberry(contacts)
     contacts.each do |person, contacts_details_hash|
     if person == "Freddy Mercury"
       contacts_details_hash.each do |attribute, data|
+        if attribute== :favorite_ice_cream_flavors
         binding.pry
       end
     end
